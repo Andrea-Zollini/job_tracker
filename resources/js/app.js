793852b33f2e6da109ea8319f5229e3d4754d7b3
andrea <andrea.zollini@proton.me>
2024-07-10 10:38:36 +0200

    initial laravel setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
-import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
